a98dd63bbb69e5b0ee474cdea5861ae1714861a7
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-02 23:46:46 +0100

    add some bundles

---
+++ redland.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+source lib/functions.sh
+source ~/.Renviron
+
+# Source API key and publish
+brew update
+deploy_new_bundles redland raptor rasqal
+deploy_old_bundles redland raptor rasqal
