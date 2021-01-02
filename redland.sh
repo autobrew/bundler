510543354dc5b6a436cae8446e7f6b3845149bfc
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-03 00:36:00 +0100

    fix redland

--- redland.sh
+++ redland.sh
@@ -4,5 +4,5 @@ source ~/.Renviron
 
 # Source API key and publish
 brew update
-deploy_new_bundles redland raptor rasqal
-deploy_old_bundles redland raptor rasqal
+#deploy_new_bundles redland raptor rasqal libtool
+deploy_old_bundles redland raptor rasqal libtool
