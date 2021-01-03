ebd3fcd539a30f0adcb0fb4428edf27f39083ec5
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-03 09:17:14 +0100

    add poppler bundle

---
+++ poppler.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+source lib/functions.sh
+source ~/.Renviron
+
+# Avoid glib dependency (from cairo)
+export deps="cairo fontconfig freetype libpng lzo pixman gettext jpeg openjpeg little-cms2 libtiff"
+export poppler_extra_files="**/share/poppler"
+export package="poppler"
+deploy_new_bundles poppler-lite
+deploy_old_bundles poppler
