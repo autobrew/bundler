2d37e03594a7754f76f02596c323942d8a6c58d6
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-05 03:08:55 +0100

    add imagemagick

--- librsvg.sh
+++ librsvg.sh
@@ -5,5 +5,5 @@ source ~/.Renviron
 # Source API key and publish
 brew update
 customdeplist="harfbuzz-lite gdk-pixbuf-static libpng freetype fontconfig pixman cairo gettext libffi pcre glib jpeg libtiff fribidi lzo pango"
-#deps=customdeplist deploy_new_bundles librsvg
+deps=$customdeplist deploy_new_bundles librsvg
 deploy_old_bundles librsvg
