46e8a19b6699c2420b887c02f80ff4eed1576e15
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-05 13:56:10 +0100

    add libtool

--- imagemagick.sh
+++ imagemagick.sh
@@ -5,6 +5,6 @@ source ~/.Renviron
 # Source API key and publish
 brew update
 export package=imagemagick
-deplist="librsvg libpng freetype fontconfig pixman cairo gettext libffi pcre glib jpeg libtiff fribidi pango libffi little-cms2 openjpeg webp"
+deplist="libtool librsvg libpng freetype fontconfig pixman cairo gettext libffi pcre glib jpeg libtiff fribidi pango libffi little-cms2 openjpeg webp"
 deps="harfbuzz-lite gdk-pixbuf-static $deplist" deploy_new_bundles "imagemagick-static"
 deps="harfbuzz gdk-pixbuf $deplist" deploy_old_bundles "imagemagick@6"
