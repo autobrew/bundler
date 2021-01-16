cd05a187ba90eeb954170384e4d126943a40c6a4
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-16 17:33:38 +0100

    cleanup

--- gdal.sh
+++ gdal.sh
@@ -9,5 +9,5 @@ export pkg_config_files="*/*/bin"
 export gdal_lite_extra_files="*/*/share/gdal"
 export proj_extra_files="*/*/share/proj"
 export deps="pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
-#deploy_new_bundles gdal-lite
+deploy_new_bundles gdal-lite
 deploy_sierra_bundle gdal
