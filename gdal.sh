66c78951a793b9f5397b45d4458d1db8a8e79c80
Jeroen Ooms <jeroenooms@gmail.com>
2021-01-17 13:19:38 +0100

    fix gdal

--- gdal.sh
+++ gdal.sh
@@ -5,7 +5,7 @@ source ~/.Renviron
 # Source API key and publish
 brew update
 export package=gdal
-export pkg_config_files="*/*/bin"
+export gdal_extra_files="*/*/share/gdal"
 export gdal_lite_extra_files="*/*/share/gdal"
 export proj_extra_files="*/*/share/proj"
 export deps="pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
