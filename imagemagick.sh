8a438bd3c9452639ef0024f38753f40ce677a4ad
Jeroen Ooms <jeroenooms@gmail.com>
2021-02-23 00:44:36 +0100

    Fix for imagemagick

--- imagemagick.sh
+++ imagemagick.sh
@@ -6,5 +6,5 @@ source ~/.Renviron
 brew update
 export package=imagemagick
 deplist="libheif libde265 x265 jasper libtool librsvg libpng freetype fontconfig pixman cairo gettext libffi pcre jpeg libtiff fribidi pango libffi little-cms2 openjpeg webp"
-deps="libraw-lite glib-lite harfbuzz-lite gdk-pixbuf-static $deplist" deploy_new_bundles "imagemagick-static"
+deps="aom libraw-lite glib-lite harfbuzz-lite gdk-pixbuf-static $deplist" deploy_new_bundles "imagemagick-static"
 deps="libraw glib harfbuzz gdk-pixbuf $deplist" deploy_old_bundles "imagemagick@6"
