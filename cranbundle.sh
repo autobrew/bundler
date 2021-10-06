00d4ea7da2244abed57c519c6d9d9bf23efc7c63
Jeroen Ooms <jeroenooms@gmail.com>
2021-10-06 12:20:58 +0200

    update gdal for cran bundle

--- cranbundle.sh
+++ cranbundle.sh
@@ -13,5 +13,5 @@ export protobuf_extra_files="*/*/bin/protoc"
 export gsl_extra_files="*/*/bin/gsl-config"
 export hdf5_extra_files="*/*/bin/h5cc */*/bin/h5c++ */*/share/hdf5"
 export netcdf_extra_files="*/*/bin/nc-config */*/bin/ncxx4-config"
-export deps="mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
+export deps="mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
 deploy_sierra_bundle gdal
--- gdal.sh
+++ gdal.sh
@@ -9,6 +9,6 @@ export gdal_extra_files="*/*/share/gdal"
 export gdal_lite_extra_files="*/*/share/gdal"
 export proj_extra_files="*/*/share/proj"
 export pkg_config_files="*/*/bin/pkg-config"
-export deps="pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
+export deps="pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
 deploy_new_bundles gdal-lite
 deploy_sierra_bundle gdal
