a0785f35826221e6e14c49815ec42a3f772326c0
Jeroen Ooms <jeroenooms@gmail.com>
2021-10-10 17:38:25 +0200

    Create cran bundle for arm64

--- cranbundle.sh
+++ cranbundle.sh
@@ -13,4 +13,10 @@ export gsl_extra_files="*/*/bin/gsl-config"
 export hdf5_extra_files="*/*/bin/h5cc */*/bin/h5c++ */*/share/hdf5"
 export netcdf_extra_files="*/*/bin/nc-config */*/bin/ncxx4-config"
 export deps="mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
+
+# Bundles for new platforms, including arm64 (untested)
+brew tap autobrew/cran
+deploy_new_bundles gdal-lite
 deploy_sierra_bundle gdal
+
+
