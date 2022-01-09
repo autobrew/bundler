c5d2c69103e46c1cfffff7c0653d365cef6d787f
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-09 23:54:58 +0100

    tesseract 5.0.1

--- tesseract.sh
+++ tesseract.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
+export deps="giflib jpeg leptonica libpng libtiff little-cms2 openjpeg webp"
 export tesseract_extra_files="**/share/tessdata"
 deploy_old_bundles tesseract
 deploy_new_bundles tesseract
