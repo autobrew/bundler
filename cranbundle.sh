d6fb4732b1d6c916211a6a314cd1aacb746d9295
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-23 22:25:13 +0100

    add cmake to cranbundle

--- cranbundle.sh
+++ cranbundle.sh
@@ -10,6 +10,8 @@ export gsl_extra_files="*/*/bin/gsl-config"
 export hdf5_extra_files="*/*/bin/h5cc */*/bin/h5c++ */*/share/hdf5"
 export netcdf_extra_files="*/*/bin/nc-config */*/bin/ncxx4-config"
 export fftw_extra_files="*/*/lib/pkgconfig"
-export deps="fftw mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
+export cmake_files="*/*/bin/cmake"
+export cmake_extra_files="*/*/share/cmake"
+export deps="cmake fftw mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
 deploy_old_bundles gdal
 deploy_new_bundles gdal-lite
