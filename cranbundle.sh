b15dddef1352dae82c87cd2c230515ff25a96e9a
Jeroen Ooms <jeroenooms@gmail.com>
2022-03-18 17:30:02 +0100

    add libpng-config to cranbundle for png package

--- cranbundle.sh
+++ cranbundle.sh
@@ -12,6 +12,7 @@ export netcdf_extra_files="*/*/bin/nc-config */*/bin/ncxx4-config"
 export fftw_extra_files="*/*/lib/pkgconfig"
 export cmake_files="*/*/bin/cmake"
 export cmake_extra_files="*/*/share/cmake"
+export libpng_extra_files="*/*/bin/*-config"
 export deps="cmake fftw mpfr pcre2 protobuf gmp gsl glpk pkg-config minizip2 szip bzip2 librttopo openssl@1.1 geos udunits json-c freexl webp unixodbc expat openjpeg netcdf hdf4 hdf5 giflib jpeg libgeotiff libpng libpq zstd xz libspatialite sqlite proj libtiff libtool libdap pcre"
 deploy_old_bundles gdal
 deploy_new_bundles gdal-lite
