15e9c15e1b5aea502c8f9baab5a7f7c1ace7b2e1
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-28 18:29:26 +0200

    add protobuf to opencv

--- opencv.sh
+++ opencv.sh
@@ -3,7 +3,7 @@ source lib/functions.sh
 
 # Source API key and publish
 export package="opencv"
-export deps="jpeg libpng libtiff tbb webp"
+export deps="jpeg libpng libtiff tbb webp protobuf"
 export opencv_extra_files="**/share/opencv4"
 export opencv_static_extra_files="**/share/opencv4"
 deploy_old_bundles opencv
