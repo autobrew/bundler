c83a87543b5ee9c222a31471bf9ef26d6d39a6d0
Jeroen Ooms <jeroenooms@gmail.com>
2023-04-10 18:55:55 +0200

    update mariadb-connector-c

--- mariadb-connector-c.sh
+++ mariadb-connector-c.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
 export package="mariadb-connector-c"
-deploy_old_bundles mariadb-connector-c
 deploy_new_bundles mariadb-connector-c-static
+merge_universal_bundles mariadb-connector-c
+deploy_old_bundles mariadb-connector-c
