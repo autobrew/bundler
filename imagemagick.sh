e0d34e8c176d42cf4f59c2e7efcda5d6e61da16f
Jeroen Ooms <jeroenooms@gmail.com>
2023-04-12 14:43:49 +0200

    update imagemagick

--- imagemagick.sh
+++ imagemagick.sh
@@ -3,4 +3,5 @@ source lib/functions.sh
 export package=imagemagick
 deplist="libheif libde265 x265 jasper libtool librsvg libpng freetype fontconfig pixman cairo gettext libffi pcre jpeg libtiff fribidi pango libffi little-cms2 openjpeg webp"
 deps="libraw glib harfbuzz gdk-pixbuf $deplist" deploy_old_bundles "imagemagick@6"
-deps="aom libraw-lite glib-lite harfbuzz-lite gdk-pixbuf-static $deplist" deploy_new_bundles "imagemagick-static"
+deps="pcre2 zstd aom-lite libraw-lite glib-lite harfbuzz-lite gdk-pixbuf-static $deplist" deploy_new_bundles "imagemagick-static"
+merge_universal_bundles imagemagick
