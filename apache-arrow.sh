a3a656579798de475b0f5b274def82c2eb19f9b4
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-05 00:33:55 +0200

    update arrow

--- apache-arrow.sh
+++ apache-arrow.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
 export package="apache-arrow"
-deploy_old_bundles apache-arrow
 deploy_new_bundles apache-arrow-static
-
+merge_universal_bundles apache-arrow
+deploy_old_bundles apache-arrow
