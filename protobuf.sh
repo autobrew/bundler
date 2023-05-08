e4ed38caa3306d199a239f1295fe65e2483099dd
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-08 14:56:00 +0200

    update protobuf

--- protobuf.sh
+++ protobuf.sh
@@ -4,5 +4,6 @@ export deps=""
 export package=protobuf
 export protobuf_extra_files="*/*/bin"
 export protobuf_static_extra_files="*/*/bin"
-deploy_old_bundles protobuf
 deploy_new_bundles protobuf-static
+merge_universal_bundles protobuf
+deploy_old_bundles protobuf
