a1d4bd545889cb310f1d80e224f6eaa6b236618b
Jeroen Ooms <jeroenooms@gmail.com>
2023-06-08 13:11:28 +0200

    Rebuild libgit2 with libssh2 1.11.0

--- libgit2.sh
+++ libgit2.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 source lib/functions.sh
 export package=libgit2
-deploy_linux_bundles libgit2-static
+export deps="libssh2 openssl@1.1"
+#deploy_linux_bundles libgit2-static
 deploy_new_bundles libgit2-static
 merge_universal_bundles libgit2
 deploy_old_bundles libgit2
