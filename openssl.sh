eb16eadaf54f4d0e7caab5550be88c9e5e9c00db
Jeroen Ooms <jeroenooms@gmail.com>
2023-07-14 15:04:30 +0200

    deploy openssl3

--- openssl.sh
+++ openssl.sh
@@ -3,6 +3,6 @@ source lib/functions.sh
 
 # Source API key and publish
 export package="openssl" 
-deploy_new_bundles "openssl@1.1"
+deploy_new_bundles "openssl-static"
 merge_universal_bundles openssl
 deploy_old_bundles "openssl@1.1"
