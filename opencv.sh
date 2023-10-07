046fba9ca7c016c7038af4ec6505798d5db05780
Jeroen Ooms <jeroenooms@gmail.com>
2023-10-08 00:13:12 +0200

    opencv now uses jpeg-turbo

--- opencv.sh
+++ opencv.sh
@@ -3,9 +3,8 @@ source lib/functions.sh
 
 # Source API key and publish
 export package="opencv"
-export deps="jpeg libpng libtiff tbb webp protobuf zstd"
+export deps="jpeg-turbo jpeg libpng libtiff tbb webp protobuf zstd"
 export opencv_extra_files="**/share/opencv4"
 export opencv_static_extra_files="**/share/opencv4"
 deploy_new_bundles opencv-static
 merge_universal_bundles opencv
-deploy_old_bundles opencv
