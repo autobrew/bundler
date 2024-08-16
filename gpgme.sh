332216a636725045befe971802d1d24dc77578b7
Jeroen Ooms <jeroenooms@gmail.com>
2024-08-16 16:50:46 +0200

    Workaround for BDR

--- gpgme.sh
+++ gpgme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
-export deps="libgpg-error libassuan gnupg@1.4"
+export deps="libgpg-error libassuan gnupg@1.4 gettext"
 export gnupg_1_4_files="*/*/bin/gpg1"
 deploy_new_bundles gpgme
 merge_universal_bundles gpgme
