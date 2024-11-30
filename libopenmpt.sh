3bd75537bb26b15995625729ef90a8edfa603d99
pepijn-devries <pepijn-devries@users.noreply.github.com>
2024-11-30 13:36:47 +0100

    Update libopenmpt.sh

--- libopenmpt.sh
+++ libopenmpt.sh
@@ -3,5 +3,5 @@ source lib/functions.sh
 brew install gcc@9
 export package="libopenmpt"
 #export deps="autobrew/cran/openmpt portaudio portaudiocpp libsndfile vorbisfile vorbis flac ogg opus mpg123 z"
-deploy_new_bundles autobrew/cran/openmpt
-merge_universal_bundles openmpt
+deploy_new_bundles libopenmpt
+merge_universal_bundles libopenmpt
