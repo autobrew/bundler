1df0585dfc8966cbf37e54256c5d8663b58d0f17
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-03 21:04:39 +0200

    new cairo

--- cairo.sh
+++ cairo.sh
@@ -2,6 +2,6 @@
 source lib/functions.sh
 
 # Use default cairo, except do not bundle the 'glib' dependency
-deps="fontconfig freetype libpng lzo pixman" deploy_new_bundles cairo
+deps="fontconfig freetype libpng lzo pixman libpng" deploy_new_bundles cairo
 merge_universal_bundles cairo
 
