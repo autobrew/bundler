95135f3b64e461f576bd9b15a4e78d04a6d415bb
Jeroen Ooms <jeroenooms@gmail.com>
2025-09-06 12:35:06 +0200

    update librsvg

--- librsvg.sh
+++ librsvg.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
-customdeplist="glib-lite harfbuzz-lite gdk-pixbuf-static libpng freetype fontconfig pixman cairo gettext libffi pcre jpeg jpeg-turbo libtiff fribidi lzo pango zstd pcre2"
-deps=$customdeplist deploy_new_bundles librsvg
+export package="librsvg"
+export deps="cairo-lite pango-lite glib-lite harfbuzz-lite gdk-pixbuf-static libpng freetype pixman gettext libffi pcre jpeg jpeg-turbo libtiff fribidi lzo zstd pcre2"
+deploy_new_bundles librsvg-lite
 merge_universal_bundles librsvg
