c90aa071fa996872146d7af857ee629b34fa5dc7
Jeroen Ooms <jeroenooms@gmail.com>
2026-09-06 15:06:37 +0200

    Use upstream fluidsynth

--- fluid-synth.sh
+++ fluid-synth.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source lib/functions.sh
 export package="fluid-synth"
-export deps="autobrew/cran/libsndfile flac glib lame libogg libvorbis mpg123 opus pcre2 portaudio gettext"
-deploy_new_bundles autobrew/cran/fluid-synth
+export deps="libsndfile flac glib lame libogg libvorbis mpg123 opus pcre2 portaudio gettext"
+deploy_new_bundles fluid-synth
 merge_universal_bundles fluid-synth
