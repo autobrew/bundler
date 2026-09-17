3b73e408aa3e0602ace29b9eee7f3251aee6fd93
Jeroen Ooms <jeroenooms@gmail.com>
2026-09-17 15:37:14 +0200

    Use our own openssl

--- libgit2.sh
+++ libgit2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source lib/functions.sh
 export package=libgit2
-export deps="libssh2 openssl@3"
+export deps="libssh2 openssl-static"
 #deploy_linux_bundles libgit2-static
 deploy_new_bundles libgit2-static
 merge_universal_bundles libgit2
